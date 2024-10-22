d8319a0a4a43085a979a802a9fdd1bd8c57f94a6
gracestalker <m276150@usna.edu>
2024-10-22 09:46:28 -0400

    background stuff

--- game.py
+++ game.py
@@ -1,11 +1,32 @@
 # Example file showing a basic pygame "game loop"
 import pygame
 
+# all of my tiles and variables
+width = 1180
+height = 600
+background = pygame.Surface((width, height))
+background.fill('white')
+water = pygame.image.load('assets/Tiles/tile_73.png') # how to prep images to be loaded onto screen
+
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((700, 700))
+screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 running = True
+
+
+tile_size = water.get_width() # get the tile size
+
+for x in range(0, width, tile_size):
+    for y in range(0, height, tile_size):
+        background.blit(water, (x,y))
+        # commented out to try again later with more tiles
+        #if x < tile_size:
+           # background.blit(grass, (x,y))
+       # elif y < (2*tile_size):
+            #background.blit(sand, (x,y))
+
+
 
 while running:
     # poll for events
@@ -15,7 +36,6 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
 
     # RENDER YOUR GAME HERE
 
